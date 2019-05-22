96b81841b9482edf33b4f098289d7fb1795d4e9f
HanKyeol Kyung <kghnkl0103@gmail.com>
2019-05-22 15:12:57 +0900

    right_answer 과 wrong_answer function으로

--- arduino_treasurebox/arduino_treasurebox.ino
+++ arduino_treasurebox/arduino_treasurebox.ino
@@ -1,5 +1,7 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
+void right_answer();
+void wrong_answer(int correct_nums);
 
 //5v -> button -> pin
 const int button_pin[5] = {9, 10, 11, 12, 13};
@@ -22,48 +24,55 @@ void setup() {
   pinMode(button_pin[4], INPUT_PULLUP);
 }
 
-// setup() 함수가 호출된 이후, loop() 함수가 호출되며,
-// 블록 안의 코드를 무한히 반복 실행됩니다.
 void loop() {
   int i;
   int button_state[5] = {digitalRead(button_pin[0]), digitalRead(button_pin[1]),
                          digitalRead(button_pin[2]), digitalRead(button_pin[3]),
-                         digitalRead(button_pin[4])
+                         digitalRead(button_pin[4])//check the status buttton normal -> 1 pressed -> 0
                         };
   for (i = 0; i < 4; i++) {//add one to the number of each position
     if (button_state[i] == 0) {
       ans[i] += 1;
-      ans[i] %= 10;
+      ans[i] %= 10;//make 0 when number become 10
     }
   }
 
-  //  Serial.print(button_state[0]);
-  //  Serial.print(button_state[1]);
-  //  Serial.print(button_state[2]);
-  //  Serial.print(button_state[3]);
-  //  Serial.println(button_state[4]);
-
-
-  lcd.setCursor(0, 0);          // 0번째 줄 0번째 셀부터 입력하게 합니다.
+  lcd.setCursor(0, 0);          //order : column,row
   lcd.print("Guess the Number!");
 
-  for (i = 0; i < 4; i++) {
+  for (i = 0; i < 4; i++) { //display 4 numbers
     lcd.setCursor(i, 1);
     lcd.print(ans[i]);
   }
-  if (button_state[4] == 0) {
-    int correct_nums=0;
-    for (i = 0; i < 4; i++) {
+  if (button_state[4] == 0) { //when button 5 pressed
+    int correct_nums = 0;
+    for (i = 0; i < 4; i++) { //4개 배열 맞나 검사
       if (ans[i] == real_ans[i])
         correct_nums++;
     }
-    if (correct_nums == 4) {
-      lcd.clear();
-      lcd.setCursor(0, 0);          
-      lcd.print("!Congraturation!");
-      delay(5000);
-    }
+    if (correct_nums == 4)
+      right_answer();
+    else
+      wrong_answer(correct_nums);
   }
 
   delay(100);
+}
+void right_answer() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("!Congraturation!");
+  delay(5000);
+  lcd.clear();
+}
+void wrong_answer(int correct_nums) {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Wrong Answer! :(");
+  lcd.setCursor(0, 1);
+  lcd.print("Correct number:");
+  lcd.setCursor(15, 1);
+  lcd.print(correct_nums);
+  delay(5000);
+  lcd.clear();
 }
